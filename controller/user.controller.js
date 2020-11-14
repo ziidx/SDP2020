ccc391d6fbea27433eae723d403ab2e1928784f7
achupanikath <achupanikath@gmail.com>
2020-11-14 11:03:52 -0500

    AES 256 Encryption set up for data stored in MongoDB

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,16 @@
 
 import express from "express";
 import sha256 from "sha256";
+const CryptoJS = require("crypto-js");
 const Members = require('../models/members');
-
 const userController = express.Router();
+
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config')[env];
 
 //Home Page application connection test
 userController.get('/', (req, res) => {
-   res.send("Welcome to NOID APP")
+   res.send("Welcome to NOID APP");
 });
 
 /**
@@ -15,7 +18,7 @@ userController.get('/', (req, res) => {
  * Add a new User to your database
  */
 userController.post("/add-user", (req, res) => {
-  const member = new Members({
+  	const member = new Members({
     UID: req.body.UID,
     Name: req.body.Name,
     Age: req.body.Age,
@@ -38,72 +41,33 @@ userController.post("/add-user", (req, res) => {
   );
 });
 
-//list of all members for development purpose
-userController.get('/members', async(req,res) => {
-  const members = await Members.find({});
-  try {
-    res.send(members);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-})
-
-//list of merchants for dev
-userController.get('/merchants', async(req,res) => {
-	const merchants = await Merchants.find({});
-  try {
-    res.send(merchants);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-})
-
-//virtual wallet for member
+/**
+ * GET/
+ * To retrive Virtual Wallet information for a given member
+ */
 userController.get('/:member/memberDB/virtualWallet', (req,res) => {
 	res.send('Here are the member Credentials')
 });
-
-//virtual wallet for merchant
+/**
+ * GET/
+ * To retrive Virtual Wallet information for a given mmerchant
+ */
 userController.get('/:merchant/merchantDB/virtualWallet', (req,res) => {
 	res.send('Here are the Merchant Credentials')
 });
-
-//for development purposes to test with dummy datbase post and subsequent http get of the same data
-userController.get('/registration/memberCredentials', (req,res) => {
-	console.log("Test")
-	const member = new Members({
-    UID: "req.body.UID",
-    Name: "req.body.Name",
-    Age: 20,
-    License: "req.body.License",
-    Expiry: 123123
-  });
-  member.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-      res.send('Done')
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-  
-});
-
+/**
+ * POST/
+ * To post values for a specific merchant once the merchant ID is set
+ */
 userController.post('/:merchant/registration/merchantCredentials', (req,res) => {
 	//'Please edit the credentials
 });
 
 /**
-member server endpoints
-**/
+ * GET/
+ * To retrive information for a given member for Merchant Side
+ */
 
-//endpoint for merchant server to request info for member
 userController.get('/:UID/information', (req,res) => {
 	//gets the question to be asked from merchant
 	//interacts with database
@@ -124,24 +88,32 @@ userController.get('/:UID/information', (req,res) => {
 });
 
 
-//endpoint to request permission to check for credentials
-userController.get('/:UID/memberFE/permission', (req,res) => {
+/**
+ * GET/
+ * To retrive permission to check for credentials
+ */
+ userController.get('/:UID/memberFE/permission', (req,res) => {
 	//gets permission from FE
 });
-
-//endpoint to retrieve credentials from the DB
+/**
+ * GET/
+ * To retrive Credentials from the DB
+ */
 userController.get('/:UID/memberDB/information', (req,res) => {
 	//gets info from database
 });
-
-
-//endpoint to retrieve credentials from Ledger
+/**
+ * GET/
+ * To retrive Credentials from the Ledger
+ */
 userController.get('/:UID/Ledger/information', (req,res) => {
 	//gets info from ledger
 });
 
-
-//endpoint to compute validity
+/**
+ * GET/
+ * To retrive Validity
+ */
 userController.get('/:UID/validation', (req,res) => {
 	//compares hash
 });
@@ -149,13 +121,81 @@ userController.get('/:UID/validation', (req,res) => {
 
 /**merchant server endpoints
 **/
+/**
+ * GET/
+ * To retrive information for a member by a specific merchant
+ */
 userController.get('/:MerchantUID/:UID/information', (req,res) => {
 	//merchant scanner hits this end point with merchant ID and member UID
 });
-
-//endpoint to pose question to member server
-userController.get('/:question/:UID/information', (req,res) => {
+/**
+ * GET/
+ * To retrive questions from Merchant
+ */
+ userController.get('/:question/:UID/information', (req,res) => {
 	//merchant poses question
+});
+
+
+
+/*********************
+
+
+DEVELOPMENT APIS
+
+
+*********************/
+
+//development API for list of all members
+userController.get('/members', async(req,res) => {
+  const members = await Members.find({});
+  try {
+  	res.send(members);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+//development API to clear stuff
+userController.get('/clear', (req,res) => {
+  Members.deleteMany({}, function(err, result){
+  	if(err){
+  		res.send(err);
+  	}else{
+  		res.send(result);
+  	}
+  });
+})
+
+//development API for list of merchants
+userController.get('/merchants', async(req,res) => {
+	const merchants = await Merchants.find({});
+  try {
+    res.send(merchants);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+//Development API to test with dummy datbase post and subsequent http get of the same data
+userController.get('/registration/memberCredentials', (req,res) => {
+	const member = new Members({
+    UID: "x",
+    Name: "req.body.Name",
+    Age: "20",
+    License: "req.body.License",
+    Expiry: "123123"
+  });
+  member.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).send(error);
+    }
+  );
+  
 });
 
 export default userController;
--- models/members.js
+++ models/members.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
+const CryptoJS = require("crypto-js");
 
+
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config')[env];
+
+//AES 256 encryption of stored values
+function encrypt(plaintext){
+	var ciphertext = CryptoJS.AES.encrypt(plaintext, config.database.encryption_secret_key).toString();
+	return ciphertext;
+}
+//AES 256 decryption of retrieved values
+function decrypt(ciphertext){
+	var bytes  = CryptoJS.AES.decrypt(ciphertext, config.database.encryption_secret_key);
+	var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+	return plaintext;
+}
 const MemberSchema = new mongoose.Schema({
-  UID: { type: String, required: true },
-  Name: { type: String, required: true },
-  Age: { type: Number, required: true },
-  License: { type: String, required: true },
-  Expiry: { type: Number, required: true },
-});
+  UID: { type: String, required: true, set: encrypt, get: decrypt },
+  Name: { type: String, required: true, set: encrypt, get: decrypt },
+  Age: { type: String, required: true, set: encrypt, get: decrypt },
+  License: { type: String, required: true, set: encrypt, get: decrypt },
+  Expiry: { type: String, required: true, set: encrypt, get: decrypt }
+},{toJSON: {getters: true}});
 
 module.exports = mongoose.model('Members', MemberSchema);
